32b4783dba6792b143ed27f9952cbd49916bd331
Minsoo Kim <ms214@ms214.kr>
2024-09-07 18:04:03 +0900

    #14 feat: 회원정보수정(기본정보)기능 추가

--- lib/MemberInfoEdit.dart
+++ lib/MemberInfoEdit.dart
@@ -1,6 +1,10 @@
+import 'package:dio/dio.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:academy_manager/AppBar.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class MemberInfoEdit extends StatefulWidget {
   String name, email, phone, id;
@@ -14,8 +18,54 @@ class MemberInfoEdit extends StatefulWidget {
 class _MemberInfoEditState extends State<MemberInfoEdit> {
   String name, email, phone, id;
   FileImage image;
+  String? accessToken, refreshToken;
+  static final storage = new FlutterSecureStorage();
+
+  var dio;
+
+  List<TextEditingController> controller = [];
+  final _globalKey = GlobalKey<FormState>();
 
   _MemberInfoEditState({required this.name, required this.email, required this.phone, required this.id, required this.image});
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    for(int i = 0; i<3; i++)
+      controller.add(new TextEditingController());
+
+    dio = new Dio();
+    dio.options.baseUrl =
+    'http://192.168.0.118:8000';
+    dio.options.connectTimeout = 5000; // 5s
+    dio.options.receiveTimeout = 3000;
+    dio.options.headers =
+    {'Content-Type': 'application/json'};
+    dio.interceptors.add(
+        InterceptorsWrapper(
+            onError: (DioError error, ErrorInterceptorHandler handler){
+              Fluttertoast.showToast(msg: error.response?.data["message"]);
+              return handler.next(error);
+            }
+        )
+    );
+
+    WidgetsBinding.instance.addPostFrameCallback((_){
+      _asyncMethod();
+    });
+
+    controller[1].text = email;
+    controller[2].text = phone;
+
+  }
+
+  _asyncMethod() async{
+    accessToken = await storage.read(key: 'accessToken');
+    refreshToken = await storage.read(key: 'refreshToken');
+
+    dio.options.headers['Authorization'] = 'Bear '+accessToken.toString();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,38 +82,64 @@ class _MemberInfoEditState extends State<MemberInfoEdit> {
               backgroundColor: Colors.grey[300],
             ),
             SizedBox(height: 8.h),
-            Text('사진 편집', style: TextStyle(fontSize: 14.sp)),
+            TextButton(
+                onPressed: (){
+
+                },
+                child: Text('사진 편집', style: TextStyle(fontSize: 14.sp))),
             SizedBox(height: 20.h),
             Container(
               padding: EdgeInsets.all(16.w),
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.black, width: 2.w),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("name: "+name, style: TextStyle(fontSize: 16.sp)),
-                  SizedBox(height: 10.h),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'password'),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 10.h),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'password 확인'),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 10.h),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Email'),
-                    initialValue: email,
-                  ),
-                  SizedBox(height: 10.h),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'phone'),
-                    initialValue: phone,
-                  ),
-                ],
+              child: Form(
+                key: _globalKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("name: "+name, style: TextStyle(fontSize: 16.sp)),
+                    SizedBox(height: 10.h),
+                    TextFormField(
+                      controller: controller[0],
+                      decoration: InputDecoration(labelText: 'password'),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 10.h),
+                    TextFormField(
+                      validator: (value){
+                        if(value != controller[0].text){
+                          return "두 비밀번호가 다릅니다.";
+                        }else return null;
+                      },
+                      decoration: InputDecoration(labelText: 'password 확인'),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 10.h),
+                    TextFormField(
+                      validator: (value){
+                        if(value.toString().isEmpty) return "email을 입력하세요";
+                        else if(!EmailValidator.validate(value.toString())){
+                          return "올바른 형식의 email을 입력하세요";
+                        }else return null;
+                      },
+                      controller: controller[1],
+                      decoration: InputDecoration(labelText: 'Email'),
+                    ),
+                    SizedBox(height: 10.h),
+                    TextFormField(
+                      validator: (value){
+                        if(value.toString().isEmpty) return "전화번호를 입력하세요";
+                        else if(!RegExp(r'^010-?([0-9]{4})-?([0-9]{4})$').hasMatch(value.toString())){
+                          // 전화번호 형식이 알맞게 입력됐는지 확인
+                          return "올바른 형식의 휴대폰번호를 입력하세요";
+                        }else return null;
+                      },
+                      controller: controller[2],
+                      decoration: InputDecoration(labelText: 'phone'),
+                    ),
+                  ],
+                ),
               ),
             ),
             Spacer(),
@@ -73,7 +149,10 @@ class _MemberInfoEditState extends State<MemberInfoEdit> {
                 backgroundColor: Color(0xFF565D6D),
               ),
               onPressed: () {
-                submit();
+                if(_globalKey.currentState!.validate()){
+                   submit();
+                }
+
               },
               child: Text('저장', style: TextStyle(color: Colors.white, fontSize: 16.sp)),
             ),
@@ -82,7 +161,24 @@ class _MemberInfoEditState extends State<MemberInfoEdit> {
       ),
     );
   }
-  submit(){
+  submit() async{
+    try {
+      var response = await dio.put('/user/' + id + '/basic-info',
+          data: {
+            "password": controller[0].text,
+            'email': controller[1].text,
+            'phone_number': controller[2].text,
+          }
+      );
+
+      if(response.statusCode == 200){
+        Fluttertoast.showToast(msg: '정상적으로 변경이 완료되었습니다.');
+        Navigator.pop(context);
+      }
+
+    }catch(err){
+      print(err);
+    }
 
   }
 }
