17df4ec288a27db1cb0b2bab8e985ced8cf04b1e
Minsoo Kim <ms214@ms214.kr>
2024-09-13 11:10:05 +0900

    #23 feat: 회원탈퇴 버튼을 앱설정으로 이동

--- lib/AppSettings.dart
+++ lib/AppSettings.dart
@@ -1,20 +1,54 @@
 import 'package:academy_manager/AppBar.dart';  // AppBar.dart 파일에서 MyAppBar를 import
+import 'package:academy_manager/MyDio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:academy_manager/main.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class AppSettings extends StatefulWidget {
-
 
   @override
   _AppSettingsState createState() => _AppSettingsState();
 }
 
 class _AppSettingsState extends State<AppSettings> {
+  String? id, accessToken;
+
+  String name="", email="";
+
+  static final dio = new MyDio();
+
+  static final storage = new FlutterSecureStorage();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_){
+      _asyncMethod();
+    });
+  }
+
+  _asyncMethod() async{
+    id = await storage.read(key: 'id');
+    accessToken = await storage.read(key: 'accessToken');
+
+    dio.addResponseInterceptor('Authorization', 'Bear '+accessToken.toString());
+
+    var response = await dio.get('/user/'+id.toString()+'/basic-info');
+    setState(() {
+      name = response.data['user_name'];
+      email = response.data['email'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    dio.addErrorInterceptor(context);
     return Scaffold(
       appBar: MyAppBar().build(context),
-      drawer: MenuDrawer(name: '현우진', email: 'example@gmail.com', subjects: ['수학']),
+      drawer: MenuDrawer(name: name, email: email, subjects: ['수학']),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
         child: Column(
@@ -73,11 +107,26 @@ class _AppSettingsState extends State<AppSettings> {
               ),
               onTap: () {
                 // 탈퇴하기 클릭 시 동작
+                delete();
               },
             ),
           ],
         ),
       ),
     );
+  }
+
+  delete() async {
+    dio.addResponseInterceptor('Content-Type', 'application/json');
+    try {
+      var response = await dio.delete('/user/'+id.toString());
+      if(response.statusCode == 200){
+        Fluttertoast.showToast(msg: "정상탈퇴 되었습니다.");
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>MyApp()), (route)=>false);
+      }
+    } catch (err) {
+      print(err);
+      Navigator.pop(context);
+    }
   }
 }
