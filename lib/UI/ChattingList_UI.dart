d0c0478564e201932b35eb78c2bfbd17f8d51619
Minsoo Kim <ms214@ms214.kr>
2024-10-01 23:23:12 +0900

    #43 feat: ListTile클릭시 해당 채팅방ui로 이동 구현

--- lib/UI/ChattingList_UI.dart
+++ lib/UI/ChattingList_UI.dart
@@ -1,4 +1,5 @@
 import 'package:academy_manager/UI/AppBar_UI.dart';
+import 'package:academy_manager/UI/ChattingRoom_UI.dart';
 import 'package:flutter/material.dart';
 
 class ChattingListUI extends StatefulWidget {
@@ -27,13 +28,13 @@ class _ChattingListUIState extends State<ChattingListUI> {
             ),
           ),
           for(int i = 0; i<4; i++)
-            showChattingRomList("사용자 이름", "마지막 채팅 내용", "날짜 or 시간")
+            showChattingRomList(context, "사용자 이름", "마지막 채팅 내용", "날짜 or 시간")
         ],
       ),
     );
   }
 
-  Widget showChattingRomList(String name, String content, String time){
+  Widget showChattingRomList(BuildContext context, String name, String content, String time){
     return ListTile(
       // leading: , //TODO: 사용자 프로필 이미지 가져오기.
       leading: Image.asset('img/default.png'), //테스트 이미지
@@ -43,6 +44,7 @@ class _ChattingListUIState extends State<ChattingListUI> {
       shape:  Border(bottom: BorderSide(color: Color(0xFFD9D9D9))),
       onTap: (){
         // TODO: 채팅방 연결.
+        Navigator.push(context, MaterialPageRoute(builder: (builder)=>ChattingRoomUI()));
       },
     );
   }
